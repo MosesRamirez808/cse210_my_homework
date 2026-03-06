8fcfc0954c3a554f53fb7a05d15b736204c3e059
Moses Ramirez <mramirez4@byupathway.edu>
2026-03-05 18:14:37 -1000

    Completed Exercises

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,13 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World");
+        Console.WriteLine("This is a C#.");
+
+        Console.Write("What is your favorite color? ");
+        string color = Console.ReadLine();
+        Console.WriteLine("Your favorite color is " + color + ".");
+
+       
+        
     }
 }
--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,70 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine("Enter your grade percentage: ");
+        int percentage = int.Parse(Console.ReadLine());
+
+        string letter = "";
+        string sign = "";
+
+        if (percentage >= 90)
+        {
+            letter = "A";
+        }
+        else if (percentage >= 80)
+        {
+            letter = "B";
+        }
+        else if (percentage >= 70)
+        {
+            letter = "C";
+        }
+        else if (percentage >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        
+        int lastDigit = percentage % 10;
+
+        if (letter != "F")
+        {
+            if (lastDigit >= 7)
+            {
+                sign = "+";
+            }
+            else if (lastDigit < 3)
+            {
+                sign = "-";
+            }
+        }
+
+        
+        if (letter == "A" && sign == "+")
+        {
+            sign = "";
+        }
+
+        if (letter == "F")
+        {
+            sign = ""; 
+        }
+
+        
+        Console.WriteLine($"Your letter grade is: {letter}{sign}");
+
+        
+        if (percentage >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed the course.");
+        }
+        else
+        {
+            Console.WriteLine("Keep working hard! You'll do better next time.");
+        }
     }
 }
