8c98e8b1977cefe4aaba3b913c06c48af1296657
Moses Ramirez <mramirez4@byupathway.edu>
2026-03-18 17:26:17 -1000

    Update

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -1,9 +1,37 @@
 using System;
 
-class Program
+namespace FractionProject
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        static void Main(string[] args)
+        {
+            
+            
+           
+            Fraction f1 = new Fraction();
+            Console.WriteLine($"Default: {f1.GetFractionString()} = {f1.GetDecimalValue()}");
+
+            
+            Fraction f2 = new Fraction(5);
+            Console.WriteLine($"Whole Number: {f2.GetFractionString()} = {f2.GetDecimalValue()}");
+
+           
+            Fraction f3 = new Fraction(3, 4);
+            Console.WriteLine($"Fraction: {f3.GetFractionString()} = {f3.GetDecimalValue()}");
+
+           
+            f3.SetTop(1);
+            f3.SetBottom(3);
+            
+            Console.WriteLine("\nAfter using Setters:");
+            Console.WriteLine($"New Fraction: {f3.GetFractionString()}");
+            Console.WriteLine($"New Decimal: {f3.GetDecimalValue()}");
+
+            
+            Console.WriteLine("\nPress any key to exit...");
+            Console.ReadKey();
+        }
     }
+
 }
