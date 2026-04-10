76c1854a484f06d49a9daa628df882786c1cb830
Moses Ramirez <mramirez4@byupathway.edu>
2026-04-09 17:19:06 -1000

    update

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,19 @@
-using System;
+using Shapes;
 
-class Program
+
+List<Shape> shapes = new List<Shape>();
+
+
+shapes.Add(new Square("Red", 5));
+shapes.Add(new Rectangle("Blue", 4, 8));
+shapes.Add(new Circle("Green", 3));
+
+
+foreach (Shape s in shapes)
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
-    }
+    
+    string color = s.GetColor();
+    double area = s.GetArea();
+
+    Console.WriteLine($"The {color} shape has an area of {area:0.00}.");
 }
---
+++ week06/Shapes/Shape.cs
@@ -0,0 +1,24 @@
+namespace Shapes;
+
+public abstract class Shape
+{
+    private string _color;
+
+    public Shape(string color)
+    {
+        _color = color;
+    }
+
+    public string GetColor()
+    {
+        return _color;
+    }
+
+    public void SetColor(string color)
+    {
+        _color = color;
+    }
+
+    // This method has no body because "Shape" doesn't have a formula.
+    public abstract double GetArea();
+}
