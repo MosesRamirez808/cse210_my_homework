4c05b3113a8d87a6b696d465462a976c66ddca82
Moses Ramirez <mramirez4@byupathway.edu>
2026-04-13 15:59:01 -1000

    Commit

---
+++ week06/EternalQuest/Goal.cs
@@ -0,0 +1,20 @@
+namespace EternalQuest;
+
+public abstract class Goal
+{
+    protected string _shortName;
+    protected string _description;
+    protected int _points;
+
+    public Goal(string name, string description, int points)
+    {
+        _shortName = name;
+        _description = description;
+        _points = points;
+    }
+
+    public abstract void RecordEvent();
+    public abstract bool IsComplete();
+    public virtual string GetDetailsString() => "";
+    public abstract string GetStringRepresentation();
+}
